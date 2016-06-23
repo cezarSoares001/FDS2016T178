b2eea996e40ed78b5314c915fc04b6d77f0d95c4
mcmor <mcmor@DESKTOP-3G3KIBU>
2016-06-23 20:39:46 -0300

    merging.

--- FDS2016T178/src/FDSMain.java
+++ FDS2016T178/src/FDSMain.java
@@ -5,10 +5,12 @@ public class FDSMain {
 		System.out.println("Hello, World! Comunitário!\n");
 		
 		//Michael da Costa Móra
+		//  Complementando.
 		System.out.println("Hello, World! Comunitário!\n");
 
 		//Insira, aqui, um comentário com seu nome
 		//    e um System.out, como no exemplo acima.
+		//    E é isto!!
 		
 	}
 
