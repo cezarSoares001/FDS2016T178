9d50eb8ace1deb8a1966d97dc7e93bb23ae648ec
mcmor <mcmor@DESKTOP-3G3KIBU>
2016-06-23 20:51:44 -0300

    Após o merge com Forgho.

--- FDS2016T178/src/FDSMain.java
+++ FDS2016T178/src/FDSMain.java
@@ -2,15 +2,19 @@
 public class FDSMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World! Comunitário!\n");
+		System.out.println("Hello, World! Comunitario!\n");
 		
 		//Michael da Costa Móra
 		//  Complementando.
-		System.out.println("Hello, World! Comunitário!\n");
+		System.out.println("Hello, World! Comunitario!\n");
 
 		//Insira, aqui, um comentário com seu nome
 		//    e um System.out, como no exemplo acima.
 		//    E é isto!!
+		
+		//Forgho - teste
+		System.out.println("Hello, World! Comunitario!\n");
+		
 		
 	}
 
