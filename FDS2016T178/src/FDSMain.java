649220a0b53295c04e89b19878afc5f3c6df1e69
15105105 <15105105@w3332012.portoalegre.pucrsnet.br>
2016-06-23 22:07:35 -0300

    aaa

--- FDS2016T178/src/FDSMain.java
+++ FDS2016T178/src/FDSMain.java
@@ -7,11 +7,6 @@ public class FDSMain {
 		//Michael da Costa Móra
 		//  Complementando.
 		System.out.println("Hello, World! Comunitario! Michael\n");
-
-		//Insira, aqui, um comentário com seu nome
-		//    e um System.out, como no exemplo acima.
-		//    E é isto!!
-	
 		
 		//Bruno Rovea
 		System.out.println("Hello, World! Ich bin Bruno!\n");
