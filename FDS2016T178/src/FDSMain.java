f5c56b4cf0aa32ca22a48a1de57204679882761c
Felipe Lopes <felipe.gross@acad.pucrs.br>
2016-06-23 22:04:55 -0300

    Corrigindo para pular linha do Guilherme

--- FDS2016T178/src/FDSMain.java
+++ FDS2016T178/src/FDSMain.java
@@ -3,44 +3,42 @@ public class FDSMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World! Comunitario!\n");
-		
-		//Michael da Costa Móra
-		//  Complementando.
+
+		// Michael da Costa Móra
+		// Complementando.
 		System.out.println("Hello, World! Comunitario! Michael\n");
 
-		//Insira, aqui, um comentário com seu nome
-		//    e um System.out, como no exemplo acima.
-		//    E é isto!!
-	
-		
-		//Bruno Rovea
+		// Insira, aqui, um comentário com seu nome
+		// e um System.out, como no exemplo acima.
+		// E é isto!!
+
+		// Bruno Rovea
 		System.out.println("Hello, World! Ich bin Bruno!\n");
-		
-		//Forgho - teste
+
+		// Forgho - teste
 		System.out.println("Hello, World! Comunitario! Forgho\n");
-		
-		//Jonata Santos
+
+		// Jonata Santos
 		System.out.println("Hello, World! Comunitario! Jonata Santos\n");
-		
-		//Victor Cauduro
+
+		// Victor Cauduro
 		System.out.println("Hello, World! Comunitario! Victor\n");
-		
-		//Felipe Lopes
+
+		// Felipe Lopes
 		System.out.println("Hello, World! Comunitario! Felipe\n");
 
-		//Jean
+		// Jean
 		System.out.println("Hello, World! Comunitario! Jean\n");
 
-		//Luiz Henrique Vieira
+		// Luiz Henrique Vieira
 		System.out.println("Hello, World! Comunitario! Luiz\n");
-		
-		//Guilherme s� faz besteira
-		System.out.println("Hello, World! Comunitario! Guilherme");
-		
-		
-		//Yasser Veleda de Andrade
+
+		// Guilherme s� faz besteira e nem pula linha
+		System.out.println("Hello, World! Comunitario! Guilherme/n");
+
+		// Yasser Veleda de Andrade
 		System.out.println("Hello, Yasser Veleda!");
-		
+
 	}
 
 }
