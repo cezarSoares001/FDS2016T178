e7648ae4f5db6aa7ae38a1e0f7720d331b0ebb0f
mcmor <mcmor@DESKTOP-3G3KIBU>
2016-08-09 20:07:02 -0300

    Versão para início dos trabalhos. Limpeza das velhas atualizações.

--- FDS2016T178/src/FDSMain.java
+++ FDS2016T178/src/FDSMain.java
@@ -3,35 +3,15 @@ public class FDSMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World! Comunitario!\n");
-		
+
+		//Acrescente um comentário e um print com seu nome, como abaixo.
+
 		//Michael da Costa Móra
 		//  Complementando.
 		System.out.println("Hello, World! Comunitario! Michael\n");
-		
-		//Bruno Rovea
-		System.out.println("Hello, World! Ich bin Bruno!\n");
 
-		// Forgho - teste
-		System.out.println("Hello, World! Comunitario! Forgho\n");
-			
-		//Felipe Lopes
-		System.out.println("Hello, World! Comunitario! Felipe\n");
 
-		//Jean
-		System.out.println("Hello, World! Comunitario! Jean\n");
 
-		//Luiz Henrique Vieira
-		System.out.println("Hello, World! Comunitario! Luiz\n");
-
-		// Guilherme
-		System.out.println("Hallo, World! Comunitario! Guilherme/n");
-
-		// Yasser Veleda de Andrade
-		System.out.println("Hello, Yasser Veleda!");
-		
-		// Paula Belardinelli
-		System.out.println("Hello, Paula!");
-		
 
 	}
 
